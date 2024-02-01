d7022f88f596667140d075460bd51e5f1417c17b
Tuan Anh Lai <tuananhlai95@gmail.com>
2024-02-01 23:22:28 +0700

    Using flurent validation Create login jwt

---
+++ eShopFlower.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using eShopFlower.Application.System.Users;
+using eShopFlower.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopFlower.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userSerice;
+
+        public UsersController(IUserService userService)
+        {
+            _userSerice = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var resultToken = await _userSerice.Authenticate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+            return Ok(new { token = resultToken });
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _userSerice.Register(request);
+            if (!result)
+            {
+                return BadRequest("Register ís unsuccessful.");
+            }
+            return Ok();
+        }
+    }
+}
--- eShopFlower.BackendApi/Program.cs
+++ eShopFlower.BackendApi/Program.cs
@@ -1,11 +1,17 @@
-using System.Text.Json.Serialization;
+using System;
 using eShopFlower.Application.Catalog.Products;
 using eShopFlower.Application.Common;
+using eShopFlower.Application.System.Users;
 using eShopFlower.Data.EF;
+using eShopFlower.Data.Entities;
 using eShopFlower.Utilities.Constants;
+using eShopFlower.ViewModels.System.Users;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,19 +20,79 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<eShopFlowerDbContext>(options =>
               options.UseSqlServer(builder.Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
 //builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddIdentity<AppUser, AppRole>()
+    .AddEntityFrameworkStores<eShopFlowerDbContext>()
+    .AddDefaultTokenProviders();
 // declare DI
 builder.Services.AddTransient<IStorageService, FileStorageService>();
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
 builder.Services.AddTransient<IManageProductsService, ManageProductService>();
+builder.Services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+builder.Services.AddTransient<IUserService, UserService>();
 
+builder.Services.AddControllers().AddFluentValidation(fw => fw.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>()); ;
 
 builder.Services.AddSwaggerGen(c =>
  {
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop Flower", Version = "v1" });
-     c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+     // c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+     {
+         Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+         Name = "Authorization",
+         In = ParameterLocation.Header,
+         Type = SecuritySchemeType.ApiKey,
+         Scheme = "Bearer"
+     });
+
+     c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                      }
+                    });
  });
 
- builder.Services.AddControllersWithViews();
+string issuer = builder.Configuration.GetValue<string>("Tokens:Issuer");
+string signingKey = builder.Configuration.GetValue<string>("Tokens:Key");
+byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = issuer,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+    };
+});
 
 var app = builder.Build();
 
@@ -41,6 +107,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
@@ -50,7 +117,6 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShopFlower V1");
 });
-
 
 app.MapControllerRoute(
     name: "default",
